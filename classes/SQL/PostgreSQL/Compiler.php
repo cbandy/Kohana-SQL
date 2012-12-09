3cf37bc7716b1536ebc8119a20def4c7c40629ea
Chris Bandy <bandy.chris@gmail.com>
2012-12-09 10:51:10 -0600

    PostgreSQL: Compiler that outputs the correct parameter references

---
+++ classes/SQL/PostgreSQL/Compiler.php
@@ -0,0 +1,109 @@
+<?php
+namespace SQL\PostgreSQL;
+
+use SQL\Compiler as SQL_Compiler;
+use SQL\Expression;
+use SQL\Identifier;
+use SQL\Statement;
+
+/**
+ * @package     SQL
+ * @subpackage  PostgreSQL
+ *
+ * @author      Chris Bandy
+ * @copyright   (c) 2011-2012 Chris Bandy
+ * @license     http://www.opensource.org/licenses/isc-license.txt
+ */
+class Compiler extends SQL_Compiler
+{
+	/**
+	 * Maximum number of bytes allowed in an identifier
+	 *
+	 * @link http://www.postgresql.org/docs/current/static/sql-syntax-lexical.html#SQL-SYNTAX-IDENTIFIERS
+	 */
+	const MAX_LENGTH_IDENTIFIER = 63;
+
+	/**
+	 * Convert a generic [Expression] into a parameterized [Statement].
+	 *
+	 * @param   Expression  $statement  SQL statement
+	 * @return  Statement
+	 */
+	public function parse_statement($statement)
+	{
+		$compiler = $this;
+		$parser = array('parameters' => array());
+
+		/**
+		 * Recursively replace array, [Expression] and [Identifier] parameters
+		 * until all parameters are unquoted literals.
+		 *
+		 * @param   Expression  $value
+		 * @return  string  SQL fragment
+		 */
+		$parser['expression'] = function ($value) use ($compiler, &$parser)
+		{
+			// An expression without parameters is just raw SQL
+			if (empty($value->parameters))
+				return (string) $value;
+
+			/**
+			 * @var array Hash of SQL fragments by named parameter
+			 */
+			$fragments = NULL;
+			$position = 0;
+
+			return preg_replace_callback(
+				$compiler->placeholder,
+				function ($matches) use (&$fragments, &$parser, &$position, $value)
+				{
+					if ($matches[0] === '?')
+					{
+						return $parser['parameter'](
+							$value->parameters[$position++]
+						);
+					}
+
+					// Named parameter
+					$param = $matches[0];
+
+					if (isset($fragments[$param]))
+						return $fragments[$param];
+
+					return $fragments[$param] = $parser['parameter'](
+						$value->parameters[$param]
+					);
+				},
+				(string) $value
+			);
+		};
+
+		/**
+		 * Recursively expand a parameter value to an SQL fragment consisting
+		 * only of placeholders.
+		 *
+		 * @param   mixed   $value  Unquoted parameter
+		 * @return  string  SQL fragment
+		 */
+		$parser['parameter'] = function (&$value) use ($compiler, &$parser)
+		{
+			if (is_array($value))
+				return implode(', ', array_map($parser['parameter'], $value));
+
+			if ($value instanceof Expression)
+				return $parser['expression']($value);
+
+			if ($value instanceof Identifier)
+				return $compiler->quote($value);
+
+			// Capture possible reference
+			$parser['parameters'][] =& $value;
+
+			return '$'.count($parser['parameters']);
+		};
+
+		$result = $parser['expression']($statement);
+
+		return new Statement($result, $parser['parameters']);
+	}
+}
---
+++ tests/unit/SQL/PostgreSQL/CompilerTest.php
@@ -0,0 +1,158 @@
+<?php
+namespace SQL\PostgreSQL;
+
+use SQL\Expression;
+use SQL\Identifier;
+use SQL\Statement;
+use SQL\Table;
+
+/**
+ * @package     SQL
+ * @subpackage  PostgreSQL
+ * @author      Chris Bandy
+ */
+class CompilerTest extends \PHPUnit_Framework_TestCase
+{
+	public function provider_parse_statement()
+	{
+		return array(
+			array(new Expression(''), new Statement('')),
+
+			// data set #1
+			array(
+				new Expression('?', array('a')),
+				new Statement('$1', array('a'))
+			),
+			array(
+				new Expression('?', array(new Expression('a'))),
+				new Statement('a')
+			),
+			array(
+				new Expression('?', array(new Identifier('a'))),
+				new Statement('"a"')
+			),
+			array(
+				new Expression('?', array(new Table('a'))),
+				new Statement('"pre_a"')
+			),
+
+			// data set #5
+			array(
+				new Expression(':a', array(':a' => 'b')),
+				new Statement('$1', array('b'))
+			),
+			array(
+				new Expression(':a', array(':a' => new Expression('b'))),
+				new Statement('b')
+			),
+			array(
+				new Expression(':a', array(':a' => new Identifier('b'))),
+				new Statement('"b"')
+			),
+			array(
+				new Expression(':a', array(':a' => new Table('b'))),
+				new Statement('"pre_b"')
+			),
+
+			// data set #9
+			array(
+				new Expression('?', array(array())),
+				new Statement('')
+			),
+			array(
+				new Expression('?', array(array('a', 'b'))),
+				new Statement('$1, $2', array('a', 'b'))
+			),
+
+			// data set #11
+			array(
+				new Expression('?', array(array(new Expression('a'), 'b'))),
+				new Statement('a, $1', array('b'))
+			),
+			array(
+				new Expression('?', array(array(new Identifier('a'), 'b'))),
+				new Statement('"a", $1', array('b'))
+			),
+			array(
+				new Expression('?', array(array(new Table('a'), 'b'))),
+				new Statement('"pre_a", $1', array('b'))
+			),
+
+			// data set #14
+			array(
+				new Expression(':a', array(':a' => array('b', new Expression('c')))),
+				new Statement('$1, c', array('b'))
+			),
+			array(
+				new Expression(':a', array(':a' => array('b', new Identifier('c')))),
+				new Statement('$1, "c"', array('b'))
+			),
+			array(
+				new Expression(':a', array(':a' => array('b', new Table('c')))),
+				new Statement('$1, "pre_c"', array('b'))
+			),
+
+			// data set #17
+			array(
+				new Expression(':a :a', array(':a' => 'b')),
+				new Statement('$1 $1', array('b'))
+			),
+			array(
+				new Expression(':a :a', array(':a' => new Expression('b'))),
+				new Statement('b b')
+			),
+			array(
+				new Expression(':a :a', array(':a' => new Identifier('b'))),
+				new Statement('"b" "b"')
+			),
+			array(
+				new Expression(':a :a', array(':a' => new Table('b'))),
+				new Statement('"pre_b" "pre_b"')
+			),
+
+			// data set #21
+			array(
+				new Expression(':a :a', array(':a' => new Expression('? ?', array('b', 'c')))),
+				new Statement('$1 $2 $1 $2', array('b', 'c'))
+			),
+		);
+	}
+
+	/**
+	 * @covers  SQL\PostgreSQL\Compiler::parse_statement
+	 *
+	 * @dataProvider    provider_parse_statement
+	 *
+	 * @param   Expression  $argument   Argument to the method
+	 * @param   Statement   $expected   Expected result
+	 */
+	public function test_parse_statement($argument, $expected)
+	{
+		$compiler = new Compiler('pre_');
+
+		$this->assertEquals($expected, $compiler->parse_statement($argument));
+	}
+
+	/**
+	 * @covers  SQL\PostgreSQL\Compiler::parse_statement
+	 */
+	public function test_parse_statement_bound()
+	{
+		$compiler = new Compiler;
+
+		$expression = new Expression('? :a');
+		$expression->bind(0, $var);
+		$expression->bind(':a', $var);
+
+		$statement = $compiler->parse_statement($expression);
+
+		$this->assertSame(
+			array(0 => NULL, 1 => NULL), $statement->parameters()
+		);
+
+		$var = 1;
+		$this->assertSame(
+			array(0 => 1, 1 => 1), $statement->parameters()
+		);
+	}
+}
