c0700a141ca2881c9471776d7aed0e197e16ec83
Chris Bandy <bandy.chris@gmail.com>
2012-12-29 23:40:48 -0600

    TestListener to ensure every test has an assertion
    This is one of the features of --strict mode

---
+++ tests/support/SQL/TestListener.php
@@ -0,0 +1,56 @@
+<?php
+namespace SQL;
+
+use PHPUnit_Framework_AssertionFailedError as AssertionFailedError;
+use PHPUnit_Framework_IncompleteTestError as IncompleteTestError;
+use PHPUnit_Framework_Test as Test;
+use PHPUnit_Framework_TestCase as TestCase;
+use PHPUnit_Framework_TestSuite as TestSuite;
+
+class TestListener implements \PHPUnit_Framework_TestListener
+{
+	public function addError(Test $test, \Exception $e, $time)
+	{
+	}
+
+	public function addFailure(Test $test, AssertionFailedError $e, $time)
+	{
+	}
+
+	public function addIncompleteTest(Test $test, \Exception $e, $time)
+	{
+	}
+
+	public function addSkippedTest(Test $test, \Exception $e, $time)
+	{
+	}
+
+	protected function assertAssertions(TestCase $test, $time)
+	{
+		if ($test->getNumAssertions() <= 0)
+		{
+			$test->getTestResultObject()->addFailure(
+				$test,
+				new IncompleteTestError('This test does not perform any assertions'),
+				$time
+			);
+		}
+	}
+
+	public function startTest(Test $test)
+	{
+	}
+
+	public function endTest(Test $test, $time)
+	{
+		$this->assertAssertions($test, $time);
+	}
+
+	public function startTestSuite(TestSuite $suite)
+	{
+	}
+
+	public function endTestSuite(TestSuite $suite)
+	{
+	}
+}
