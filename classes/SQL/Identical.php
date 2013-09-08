ecb1ddcf9d49b5d48a7122612bdc7049a9355735
Chris Bandy <bandy.chris@gmail.com>
2013-09-08 09:50:58 -0500

    Mention IS DISTINCT FROM
    Closes GH-31

--- classes/SQL/Identical.php
+++ classes/SQL/Identical.php
@@ -9,11 +9,14 @@ namespace SQL;
  * operands are equal or null. Otherwise, this comparator yields true when the
  * operands are not equal or one of them is null.
  *
+ * This is similar to `IS DISTINCT FROM` of SQL:1999 but doesn't handle
+ * interesting types like rows, arrays or multisets.
+ *
  * @package     SQL
  * @category    Expressions
  *
  * @author      Chris Bandy
- * @copyright   (c) 2011-2012 Chris Bandy
+ * @copyright   (c) 2011-2013 Chris Bandy
  * @license     http://www.opensource.org/licenses/isc-license.txt
  */
 class Identical extends Expression
